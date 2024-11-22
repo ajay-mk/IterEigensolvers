47eea0cbcb77fd55bce61ec4c935efcaf895f80e
Ajay <ajaymk22@vt.edu>
2024-11-22 18:40:57 -0500

    pre-commit hooks, and first run

--- src/davidson.h
+++ src/davidson.h
@@ -5,36 +5,29 @@
 #ifndef DAVIDSON_H
 #define DAVIDSON_H
 
+#include <exception>
 #include <iostream>
 #include <vector>
-#include <exception>
 
-
-/// @brief Base clas for iterative eigensolvers
+/// @brief Base class for iterative eigensolvers
 ///
 /// This class is the base class for the Davidson eigensolver. It provides the
 /// interface for eigensolver and the necessary functions.
 /// All derived classes must implement the pure virtual functions.
 class IterSolver {
-public:
+ public:
   IterSolver() = default;
   virtual ~IterSolver() = default;
 
   /// @brief Set the number of roots to be computed
   virtual void set_n_roots(const size_t n_roots) {
-
-
     throw std::runtime_error("Not implemented");
   }
 
   /// @brief return eigenvalues
-  virtual void eigenvalues() {
-    throw std::runtime_error("Not implemented");
-  }
+  virtual void eigenvalues() { throw std::runtime_error("Not implemented"); }
   /// @brief return eigenvectors
-  virtual void eigenvectors() {
-    throw std::runtime_error("Not implemented");
-  }
+  virtual void eigenvectors() { throw std::runtime_error("Not implemented"); }
 };
 
 /// @brief Davidson eigensolver
@@ -43,10 +36,9 @@ public:
 
 template <typename MatrixType>
 class DavidsonLiu : public IterSolver {
-public:
+ public:
   DavidsonLiu() = delete;
   ~DavidsonLiu() override = default;
 };
 
-
-#endif //DAVIDSON_H
+#endif  // DAVIDSON_H
